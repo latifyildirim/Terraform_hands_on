99c7f4ca6f939871ed3052cddff29210a3cb4c21
latif <lyildirim.3017@gmail.com>
2023-03-22 12:50:34 +0100

    variables.tf eklendi

---
+++ Terraform_Commands_Varables_Conditonals_Loops/Calisma-dosyalari/variables.tf
@@ -0,0 +1,29 @@
+
+variable "ec2-name" {
+  default = "latif-ec2"
+}
+
+variable "ec2-ami" {
+  default = "ami-0742b4e673072066f"
+}
+
+variable "ec2-type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "latif"
+}
+
+variable "s3_bucket_name" {
+  # default = "latiff-s3-bucket-variable-addwhateveryouwant"
+}
+
+variable "num_of_buckets" {
+  default = 2
+
+}
+
+variable "users" {
+  default = ["santiano", "maichael", "faredo"]
+}
