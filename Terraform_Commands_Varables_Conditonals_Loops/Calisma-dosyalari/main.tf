dcc5151a52aa8d64bb4b6b3b3b2c60d88fd5367b
latif <lyildirim.3017@gmail.com>
2023-03-22 12:49:30 +0100

    main.tf

---
+++ Terraform_Commands_Varables_Conditonals_Loops/Calisma-dosyalari/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.59.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  mytag = "latif-local-name"
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = var.ec2-ami
+  instance_type = var.ec2-type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "${local.mytag}-come from locals"
+  }
+} 
+
+resource "aws_s3_bucket" "tf-s3" {
+  # bucket = "${var.s3_bucket_name}-${count.index}"
+  # # count  = var.num_of_buckets
+  # count = var.num_of_buckets != 0 ? var.num_of_buckets : 3
+  for_each = toset(var.users)
+  bucket   = "example-tf-s3-bucket-${each.value}"
+}
+
+resource "aws_iam_user" "new-users" {
+  for_each = toset(var.users)
+  name = each.value
+}
