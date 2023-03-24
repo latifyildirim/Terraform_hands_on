5fd72c06fb50ac3ebbb09ef47cc1664ba8fd6bb4
latif <lyildirim.3017@gmail.com>
2023-03-24 10:40:28 +0100

    main.tf degisiklik

--- Data_Sources_Remote_Backend_Provisioners/Provisioners/main.tf
+++ Data_Sources_Remote_Backend_Provisioners/Provisioners/main.tf
@@ -1,64 +1,8 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "4.59.0"
-#     }
-#   }
-# }
-
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-# resource "aws_instance" "instance" {
-#   ami           = "ami-0c02fb55956c7d316"
-#   instance_type = "t2.micro"
-#   key_name = "latif"
-#   security_groups = [ "value" ]
-
-#   provisioner "local-exec" {
-#     command = "${self.private_ip}"
-
-#   tags = {
-#     Name = "terraform-instance-with-provisioner"
-#   }
-# }
-
-# resource "aws_security_group" "tf-sec-gr" {
-#   name        = "tf-provisioner-sg" 
-#   vpc_id      = aws_vpc.main.id
-
-#   ingress { 
-#     from_port        = 80
-#     to_port          = 80
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"] 
-#   }
-#   ingress { 
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"] 
-#   }
-
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"] 
-#   }
-
-#   tags = {
-#     Name = "allow_tls"
-#   }
-# }
-
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.59.0"
+      source  = "hashicorp/aws"
+      version = "4.60.0"
     }
   }
 }
@@ -68,20 +12,23 @@ provider "aws" {
 }
 
 resource "aws_instance" "instance" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-  key_name = "latif"
-  security_groups = [ "tf-provisioner-sg" ]
+  ami             = "ami-0c02fb55956c7d316"
+  instance_type   = "t2.micro"
+  key_name        = "latif"
+  security_groups = ["provisioner-sg"]
 
+  tags = {
+    Name = "Provisioner-ile-yaratildi"
+  }
   provisioner "local-exec" {
-    command = "echo http://${self.public_ip} >> public_ip.txt"
+    command = "echo http://${self.public_ip} >> public_ips.txt"
   }
 
   connection {
-    type     = "ssh"
-    user     = "ec2-user"
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = self.public_ip
     private_key = file("~/latif.pem")
-    host     = self.public_ip
   }
 
   provisioner "remote-exec" {
@@ -91,32 +38,31 @@ resource "aws_instance" "instance" {
       "sudo systemctl start httpd"
     ]
   }
-
   provisioner "file" {
     content     = self.public_ip
-    destination = "/home/ec2-user/public_ip.txt"
-  }
-
-  tags = {
-    Name = "terraform-instance-with-provisioner"
+    destination = "/home/ec2-user/public_ips.txt"
   }
 }
 
-resource "aws_security_group" "tf-sec-gr" {
-  name        = "tf-provisioner-sg"
+
+
+resource "aws_security_group" "provisioner-sg" {
+  name        = "provisioner-sg"
+  description = "Allow ssh-http inbound traffic"
 
   ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -124,6 +70,10 @@ resource "aws_security_group" "tf-sec-gr" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
+  tags = {
+    Name = "allow_ssh-http"
+  }
 }
